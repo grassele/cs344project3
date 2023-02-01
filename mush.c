eb9332a169e0d4402a5c9ff3aa059f8cfbc4a642
grassele <91402260+grassele@users.noreply.github.com>
2023-01-31 21:49:27 -0800

    working mush shell

--- mush.c
+++ mush.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-    for (int i = 0; i < 2; i++) {  // TODO: delete condition for infinite loop
+    for (int i = 0; ; i++) {  
         printf("mush$ ");
         fflush(stdout);
         char com_line[2048];
@@ -18,19 +18,42 @@ int main(void)
         int i = 0;
         char *tok;
         if ((tok = strtok(com_line, " \t\n\r")) != NULL) {
-        do {
-            args[i] = tok;
-            i++;
-        } while ((tok = strtok(NULL, " \t\n\r")) != NULL);
-        args[i] = NULL;
-    }
-        // verify args array contains what it's supposed to
-        printf("number of arguments = %d\n", i+1);
-        for (int j = 0; j <= i; j++) {
-            printf("arg %d: %s\n", j, args[j]);
+            do {
+                args[i] = tok;
+                i++;
+            } while ((tok = strtok(NULL, " \t\n\r")) != NULL);
+            args[i] = NULL;
+        }
+        else { 
+            printf("not a command\n");
         }
 
+        if (!strcmp(args[0], "cd")) {
+            int i = chdir(args[1]);
+            if (i == -1) {
+                perror("chdir");
+            }
+        }
+        else if (!strcmp(args[0], "exit")) {
+            exit(0);
+        }
+        else {
+            pid_t pid = fork();
+            if (pid == 0) {
+                execvp(args[0], args);
+                perror("exec");
+                exit(1);
+            } else {
+                wait(NULL);
+            }
+        }
 
     }
 
 }
+
+// verify args array contains what it's supposed to
+    // printf("number of arguments = %d\n", i+1);
+    // for (int j = 0; j <= i; j++) {
+    //     printf("arg %d: %s\n", j, args[j]);
+    // }
